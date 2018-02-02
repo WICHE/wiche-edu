bca057b8c77fb12ec6e7ea661aa18bb2c00b28c9
WICHEwebDev <webdev@wiche.edu>
2018-02-02 12:47:07 -0700

    secure google fonts

--- sites/all/themes/zen_wiche/templates/html.tpl.php
+++ sites/all/themes/zen_wiche/templates/html.tpl.php
@@ -25,10 +25,10 @@
   <?php endif; ?>
   <meta http-equiv="cleartype" content="on">
   <meta name="google-site-verification" content="8zD4FAN372NM_0YaWQZVAtmMtALVI8IuPaINdP5ZSPE" />
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,300|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Lora:700,400' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,300|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lora:700,400' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <?php if ($add_respond_js): ?>
