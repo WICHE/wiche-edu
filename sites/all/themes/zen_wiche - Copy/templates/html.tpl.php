b168ffe0937c90578b427f179386f6d336573a99
WICHEwebDev <webdev@wiche.edu>
2018-02-12 12:09:09 -0700

    revert css

---
+++ sites/all/themes/zen_wiche - Copy/templates/html.tpl.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the basic html structure of a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see http://drupal.org/node/1728208
+ */
+?><!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+
+<head>
+	<a name="page-top"></a>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+
+  <?php if ($default_mobile_metatags): ?>
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width">
+  <?php endif; ?>
+  <meta http-equiv="cleartype" content="on">
+  <meta name="google-site-verification" content="8zD4FAN372NM_0YaWQZVAtmMtALVI8IuPaINdP5ZSPE" />
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,300|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lora:700,400' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <?php if ($add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5-respond.js"></script>
+    <![endif]-->
+  <?php elseif ($add_html5_shim): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5.js"></script>
+    <![endif]-->
+  <?php endif; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N2ZXGT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N2ZXGT');</script>
+<!-- End Google Tag Manager -->
+  <?php if ($skip_link_text && $skip_link_anchor): ?>
+    <p id="skip-link">
+      <a href="#main-nav" class="element-invisible element-focusable"><?php print t('Skip to main navigation') ?></a>
+    </p>
+  <?php endif; ?>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  
+<script>
+      var navigation = responsiveNav(".nav-collapse");
+</script> 
+<script type="text/javascript" src="http://aws.predictiveresponse.net/http/trac.js"> </script>
+<script type="text/javascript"> trackPredictive(); </script>
+</body>
+</html>
---
+++ sites/all/themes/zen_wiche - Copy/templates/page--front.tpl.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see http://drupal.org/node/1728148
+ */
+?>
+
+<div id="page" class="front-page">
+<div id="tag">
+  <?php if ($site_slogan): ?>
+  <h2 class="header--site-slogan" id="site-slogan"><?php print $site_slogan; ?></h2>
+  <?php endif; ?>
+</div>
+<header class="header" id="header" role="banner">
+<div style="overflow:hidden;">
+  <?php if ($logo): ?>
+  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header--site-link" rel="home"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header--logo-image" /></a>
+  <?php endif; ?>
+  <?php if ($site_name || $site_slogan): ?>
+  <div class="header--name-and-slogan" id="name-and-slogan">
+    <?php if ($site_name): ?>
+    <h1 class="header--site-name" id="site-name"><?php print $site_name; ?></h1>
+    <?php endif; ?>
+  </div>
+  <!-- end .header--name-and-slogan -->
+  <?php endif; ?>
+  <div id="secondary-menu" class="header--secondary-menu"> <?php print render($page['log_in']); ?></div>
+  
+  <!--social media-->
+  <ul class="social-media">
+    <li class="facebook"><a href="https://www.facebook.com/wiche.edu" target="_blank"><img src="/sites/all/themes/zen_wiche/images/facebook.png" alt="Follow us on Facebook" width="16" height="16" /></a></li>
+    <li class="twitter"><a href="https://twitter.com/wicheEDU" target="_blank"><img src="/sites/all/themes/zen_wiche/images/twitter.png" alt="Follow us on Twitter" width="16" height="16" /></a></li>
+    <!--<li class="delicious"><a href="https://delicious.com/wiche_edu" target="_blank"><img src="/sites/all/themes/zen_wiche/images/delicious.png" alt="Our Delicious Links" width="16" height="16" /></a></li>-->
+  </ul>
+  <!--end social media--> 
+  </div> 
+  <div class="phone-nav">
+   <nav class="nav-collapse"><?php print render($page['phone_nav']); ?>
+   </nav>
+   </div>
+  
+  
+  <?php print render($page['header']); ?> </header>
+<div id="main-front">
+  <div id="slideshow">
+  <div class="slide-row">
+  <div class="slides"><?php print render($page['slides']); ?></div>
+  <div class="wiche-mission-self-id"><?php print render($page['self-id']); ?></div>
+  </div><!--end slide row-->
+  </div><!--end slideshow-->
+  <a id="main-content"></a>
+  <div id="programs-departments">
+    <?php print render($page['publications']); ?></div>
+    
+  <div id="content" class="column" role="main"> <?php print render($page['help']); ?>
+  <div class="promoted"><?php print render($page['highlighted']); ?></div>
+  <?php print $breadcrumb; ?>
+  <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+    <h1 class="page--title title" id="page-title"><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?> <?php print $messages; ?> <?php print render($tabs); ?> 
+    <?php if ($action_links): ?>
+    <ul class="action-links">
+      <?php print render($action_links); ?>
+    </ul>
+    <?php endif; ?>
+    <?php print render($page['main-page-content']); ?>
+     </div>
+  <!-- end #content-->
+  
+  <div id="navigation" role="navigation">
+	<a name="main-nav" tabindex="-1"></a>
+      <?php print render($page['navigation']); ?>
+ 
+    </div>
+  <!-- end #navigation -->
+  
+  <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+  <?php if ($sidebar_first || $sidebar_second): ?>
+  <aside class="sidebars"> <?php print $sidebar_first; ?> <?php print $sidebar_second; ?> </aside>
+  <?php endif; ?>
+</div>
+<!-- end #main-front -->
+
+<footer id="footer" class="<?php print $classes; ?> clearfix"> 
+  
+  <div class="header-foot"> <img id="foot-logo" src="<?php print base_path() . path_to_theme(); ?>/images/wiche-logo-footer.png" width="261" height="41" alt="WICHE, Western Interstate Commision For Higher Education" />
+    <h2 id="foot-tag">Access. Collaboration. Innovation.</h2>
+    <div class="foot-menu"> <?php print render($page['footer_menu']); ?> </div>
+    <div class="footer-columns">
+      <div class = "copyright"> <?php print render($page['copy_right']); ?> </div>
+    </div>
+  </div>
+  <!--end header-foot-->
+  
+</footer>
+ </div>
+  <!-- end #page --> 
+<?php print render($page['bottom']); ?> 
