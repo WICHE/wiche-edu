c04de72cad537f1ad87243ca4713ab49e9dc1a64
WICHEwebDev <webdev@wiche.edu>
2018-05-31 13:48:09 -0600

    GDPR Consent Module

---
+++ sites/all/modules/contrib/gdpr_consent/gdpr_consent.api.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+
+/**
+ * @defgroup gdpr_consent module integrations.
+ *
+ * Module integrations with the gdpr_consent module.
+ */
+
+/**
+ * @defgroup gdpr_consent_hooks hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend gdpr_consent.
+ */
+
+/**
+ * Runs actions when GDPR Consent is accepted
+ *
+ * @param $data
+ *   Data relating to the current acceptance. Includes:
+ *   uid - The ID of the user that accepted the terms.
+ *   version - The version of Terms accepted.
+ *   revision - The revision of Terms accepted.
+ *   accepted - The timestamp the Terms were accepted at.
+ *   language - The current language.
+ */
+function hook_gdpr_consent_accepted($data) {
+  if (module_exists('rules')) {
+    $user       = user_load($data['uid']);
+    $conditions = gdpr_consent_get_conditions($data['language']);
+    rules_invoke_event('gdpr_consent_accepted', $user, $conditions, $data['accepted']);
+  }
+}
