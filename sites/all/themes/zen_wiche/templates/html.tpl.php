21f58c9bf770985e20b6d6594f0def718fafb7e9
WICHEwebDev <webdev@wiche.edu>
2018-06-05 11:41:41 -0600

    adjust Google Tag Manager

--- sites/all/themes/zen_wiche/templates/html.tpl.php
+++ sites/all/themes/zen_wiche/templates/html.tpl.php
@@ -19,6 +19,11 @@
   <title><?php print $head_title; ?></title>
   <script type="text/javascript" src="https://aws.predictiveresponse.net/trac.js"> </script>
   <script type="text/javascript"> trackPredictive(); </script>
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N2ZXGT');</script>
 
   <?php if ($default_mobile_metatags): ?>
     <meta name="MobileOptimized" content="width">
@@ -54,12 +59,7 @@
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N2ZXGT"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2ZXGT');</script>
-<!-- End Google Tag Manager -->
+	<!-- End Google Tag Manager -->
   <?php if ($skip_link_text && $skip_link_anchor): ?>
     <p id="skip-link">
       <a href="#main-nav" class="element-invisible element-focusable"><?php print t('Skip to main navigation') ?></a>
