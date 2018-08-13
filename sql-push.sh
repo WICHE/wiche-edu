86551f3e9be6ec745ce103136613dc4c528a8dc1
Elliot Schlegelmilch <elliot@amazee.io>
2018-08-13 16:42:19 -0700

    update to lagoon tmp location.

--- sql-push.sh
+++ sql-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $AMAZEEIO_WEBROOT
+AMAZEEIO_TMP_PATH=/tmp
 
 echo "Dumping Database locally"
 drush sql-dump --result-file=$AMAZEEIO_TMP_PATH/dump_sqlpush.sql
@@ -8,10 +8,10 @@ echo "Cleaning up Trigger Definer Queries"
 sed -i 's/DEFINER=[^*]*\*/\*/g' $AMAZEEIO_TMP_PATH/dump_sqlpush.sql
 
 echo "Copy Database to remote $1"
-drush rsync -y $AMAZEEIO_TMP_PATH/dump_sqlpush.sql $1:~/tmp/dump_sqlpush.sql
+drush rsync -y $AMAZEEIO_TMP_PATH/dump_sqlpush.sql $1:/tmp/dump_sqlpush.sql
 
 echo "Import Database on remote $1"
-drush $1 ssh drush sql-cli < ~/tmp/dump_sqlpush.sql
+drush $1 ssh drush sql-cli < /tmp/dump_sqlpush.sql
 
 echo "Removing remote dump file on $1"
-drush $1 ssh rm ~/tmp/dump_sqlpush.sql
+drush $1 ssh rm /tmp/dump_sqlpush.sql
