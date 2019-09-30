c6cdecb6be858c8f9f38890a1de78fde8070199f
WICHEwebDev <33733553+WICHEwebDev@users.noreply.github.com>
2019-09-30 13:11:43 -0600

    benchmarks theme work

--- sites/all/themes/zen_wiche/templates/page--benchmarks2019.tpl.php
+++ sites/all/themes/zen_wiche/templates/page--benchmarks2019.tpl.php
@@ -1,147 +1,108 @@
 <?php
 /**
  * @file
- * Contains the theme's functions to manipulate Drupal's default markup.
+ * Returns the HTML for a single Drupal page.
  *
  * Complete documentation for this file is available online.
- * @see http://drupal.org/node/1728096
+ * @see http://drupal.org/node/1728148
  */
+?>
 
+<div id="page" class="benchmarks2019">
+<div id="tag"> <?php if ($site_slogan): ?>
+          <h2 class="header--site-slogan" id="site-slogan"><?php print $site_slogan; ?></h2>
+        <?php endif; ?></div>
+  <header class="header" id="header" role="banner">
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header--site-link" rel="home"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header--logo-image" /></a>
+    <?php endif; ?>
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header--name-and-slogan" id="name-and-slogan">
+        <?php if ($site_name): ?>
+          <h1 class="header--site-name" id="site-name">
+            <?php print $site_name; ?>
+          </h1>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
 
-/**
- * Override or insert variables into the maintenance page template.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("maintenance_page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_maintenance_page(&$variables, $hook) {
-  // When a variable is manipulated or added in preprocess_html or
-  // preprocess_page, that same work is probably needed for the maintenance page
-  // as well, so we can just re-use those functions to do that work here.
-  zen_wiche_preprocess_html($variables, $hook);
-  zen_wiche_preprocess_page($variables, $hook);
-}
-// */
+    <div id="secondary-menu" class="header--secondary-menu"> <?php print render($page['log_in']); ?></div>
+ <!--social media-->
+    <ul class="social-media" style="width:48px!important;">
+   <!-- <li class="facebook"><a href="https://www.facebook.com/wiche.edu" target="_blank"><img src="/sites/all/themes/zen_wiche/images/facebook.png" alt="Follow us on Facebook" width="16" height="16" /></a></li>-->
+    <li class="twitter"><a href="https://twitter.com/wicheEDU" target="_blank"><img src="/sites/all/themes/zen_wiche/images/twitter.png" alt="Follow us on Twitter" width="16" height="16" /></a></li>
+    <!--<li class="delicious"><a href="https://delicious.com/wiche_edu" target="_blank"><img src="/sites/all/themes/zen_wiche/images/delicious.png" alt="Our Delicious Links" width="16" height="16" /></a></li>-->
+  </ul>
+  <!--end social media--> 
 
-/**
- * Override or insert variables into the html templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("html" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+<div class="phone-nav">
+   <nav class="nav-collapse"> <?php print render($page['phone_nav']); ?>
+   </nav>
+   </div>
+   
+    <?php print render($page['header']); ?>
 
-  // The body tag's classes are controlled by the $classes_array variable. To
-  // remove a class from $classes_array, use array_diff().
-  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
-}
-// */
+  </header>
+  <div id="main">
+    <div id="full-rotator"> <?php print render($page['slideshow']); ?></div>
+    <div id="content" class="column" role="main">
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page--title title" id="page-title" style="margin-left:16px;"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php print render($page['highlighted']); ?>
+      <?php print render($page['content']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print $feed_icons; ?>
+    </div>
 
-/**
- * Override or insert variables into the page templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
-
-/**
- * Override or insert variables into the node templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("node" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // Optionally, run node-type-specific preprocess functions, like
-  // zen_wiche_preprocess_node_page() or zen_wiche_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $variables['node']->type;
-  if (function_exists($function)) {
-    $function($variables, $hook);
-  }
-}
-// */
-
-/**
- * Override or insert variables into the comment templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("comment" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_comment(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
-
-/**
- * Override or insert variables into the region templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("region" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_region(&$variables, $hook) {
-  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
-  //if (strpos($variables['region'], 'sidebar_') === 0) {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
-  //}
-}
-// */
-
-/**
- * Override or insert variables into the block templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("block" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function zen_wiche_preprocess_block(&$variables, $hook) {
-  // Add a count to all the blocks in the region.
-  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
-
-  // By default, Zen will use the block--no-wrapper.tpl.php for the main
-  // content. This optional bit of code undoes that:
-  //if ($variables['block_html_id'] == 'block-system-main') {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
-  //}
-}
-// */
-
-/**
- * Tell Drupal 7 to look for a custom page template for each node type. 
- */
-
-
+    <div id="navigation">
+	<a name="main-nav" tabindex="-1"></a>
+      <?php print render($page['navigation']); ?>
  
-// preprocess page function from https://drupal.org/node/1035656 JHF
+    </div>
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
 
-//updated template.php code
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
 
-  function zen_wiche_preprocess_page(&$variables) { 
-    $variables['theme_hook_suggestions'][] = 'page__'. $variables['node']->type;
-  }
-  
+  </div>
+
+ <footer id="footer" class="<?php print $classes; ?> clearfix">
+    
+    <div class="header-foot">
+       <img id="foot-logo" src="<?php print base_path() . path_to_theme(); ?>/images/wiche-logo-footer.png" width="261" height="41" alt="WICHE, Western Interstate Commision For Higher Education" />
+    <h2 id="foot-tag">Access. Collaboration. Innovation.</h2>
+        
+        	<div class="foot-menu">
+            	 <?php print render($page['footer_menu']); ?>
+            </div>
+        <div class="footer-columns">
+        		<div class = "copyright">
+                  <?php print render($page['copy_right']); ?>
+   </div>          
+              </div>             
+        </div><!--end header-foot-->
+           
+  </footer>
+ </div>
+</div><!-- end #page -->
+             
+<?php print render($page['bottom']); ?>
+
